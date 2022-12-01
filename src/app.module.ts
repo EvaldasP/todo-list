4dd202dba0581b73b5fbdc827513e6574aabeee3
Evaldas Pocius <evaldukas96@gmail.com>
2022-12-01 23:10:49 +0200

    feat(user-authentication): JWT auth strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,11 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/todo-list'), UsersModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/todo-list'),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { UserSchema } from '../users/users.schema';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.auth';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule,
+    UsersModule,
+    JwtModule.register({
+      secret: 'secretKey',
+      signOptions: { expiresIn: '60s' },
+    }),
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
+  ],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
+})
 export class AuthModule {}
