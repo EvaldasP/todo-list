5c96bb2e52ebda21ae16900c8dc2283d7ddae1e1
Evaldas Pocius <evaldukas96@gmail.com>
2022-12-01 23:10:26 +0200

    feat(user-authentication): users controller and service improvements

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,8 @@
 import { Body, Controller, Post, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './users.schema';
-import * as bcrypt from 'bcrypt';
 
-@Controller('auth')
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
@@ -12,9 +11,6 @@ export class UsersController {
     @Body('password') password: string,
     @Body('username') username: string,
   ): Promise<User> {
-    const saltOrRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
-    const result = await this.usersService.createUser(username, hashedPassword);
-    return result;
+    return await this.usersService.createUser(username, password);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './users.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -9,9 +10,12 @@ export class UsersService {
     @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
   ) {}
   async createUser(username: string, password: string): Promise<User> {
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+
     return this.userModel.create({
       username,
-      password,
+      password: hashedPassword,
     });
   }
   async getUser(query: object): Promise<User> {
